73840f16f249de773bbd4300c6c64e8a7a5f5935
official.pumking.dev <121762735+vPumpking@users.noreply.github.com>
2024-07-12 17:02:47 +0200

    Update plugin.gd

--- addons/mesh_lod_generator/plugin.gd
+++ addons/mesh_lod_generator/plugin.gd
@@ -2,7 +2,7 @@
 extends EditorPlugin
 
 func _enter_tree():
-  add_custom_type("MeshLODGenerator", "Node", preload("mesh_lod_generator.gd", preload("icon.svg"))
+  add_custom_type("MeshLODGenerator", "Node", preload("mesh_lod_generator.gd"), preload("icon.svg"))
 
 func _exit_tree():
   remove_custom_type("MeshLODGenerator")
